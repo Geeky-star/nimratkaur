5f926e575edf79db34a295acdb701ea532d3934e
Geeky-star <nimratkaur540@gmail.com>
2025-01-23 21:09:36 -0500

    updated exp

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -7,29 +7,6 @@ export default function Education() {
     <div className="education-container">
       <span className="education-heading">My Education</span>
       
-      {/* George Brown College */}
-      <div className="education-box">
-        <div className="education-box-item">
-          <img src={gbc} alt="George Brown College" className="education-image" />
-          <div className="education-details">
-            <div className="education-title-gpa">
-              <span className="education-title">George Brown College</span>
-              <p className="education-gpa">GPA: 4/4</p>
-            </div>
-            <p className="education-program">Master in Information Technology</p>
-          {/*  <p className="education-date">January 2024 - Present</p> */}
-          </div>
-        </div>
-        <div className="education-description">
-          <ul className="education-list">
-            <li>Mastered advanced optimization techniques, including algorithmic complexity analysis and space-time tradeoffs, to design efficient and scalable software solutions.</li>
-            <li>Acquired hands-on experience in designing and building full-fledged software solutions from concept to deployment, applying best practices for both frontend and backend development.</li>
-            <li>Gained proficiency in building robust and fault-tolerant systems, implementing redundancy, error handling, and recovery techniques to ensure high availability and reliability.</li>
-            <li>Learned to apply load balancing, caching strategies, and database indexing to enhance system performance and ensure quick response times in high-traffic applications.</li>
-         <li>Enhanced problem-solving skills through hands-on experience with real-world scenarios, enabling the development of scalable architectures using microservices and modern frameworks.</li>
-          </ul>
-        </div>
-      </div>
       
       {/* IIIT Bhopal */}
       <div className="education-box">
@@ -40,7 +17,7 @@ export default function Education() {
               <span className="education-title">Indian Institute of Information Technology, Bhopal</span>
               <p className="education-gpa">GPA: 9.1/10</p>
             </div>
-            <p className="education-program">Bachelor of Technology, Information Technology (IT)</p>
+            <p className="education-program">Bachelor of Technology, Computer Science</p>
             {/* <p className="education-date">August 2018 - May 2022</p>*/ }
           </div>
         </div>
@@ -55,3 +32,31 @@ export default function Education() {
     </div>
   );
 }
+
+/*
+George brown college
+<div className="education-box">
+<div className="education-box-item">
+  <img src={gbc} alt="George Brown College" className="education-image" />
+  <div className="education-details">
+    <div className="education-title-gpa">
+      <span className="education-title">George Brown College</span>
+      <p className="education-gpa">GPA: 4/4</p>
+    </div>
+    <p className="education-program">Master in Information Technology</p>
+ 
+  </div>
+</div>
+<div className="education-description">
+  <ul className="education-list">
+    <li>Mastered advanced optimization techniques, including algorithmic complexity analysis and space-time tradeoffs, to design efficient and scalable software solutions.</li>
+    <li>Acquired hands-on experience in designing and building full-fledged software solutions from concept to deployment, applying best practices for both frontend and backend development.</li>
+    <li>Gained proficiency in building robust and fault-tolerant systems, implementing redundancy, error handling, and recovery techniques to ensure high availability and reliability.</li>
+    <li>Learned to apply load balancing, caching strategies, and database indexing to enhance system performance and ensure quick response times in high-traffic applications.</li>
+ <li>Enhanced problem-solving skills through hands-on experience with real-world scenarios, enabling the development of scalable architectures using microservices and modern frameworks.</li>
+  </ul>
+</div>
+</div>
+
+
+*/
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,118 +1,61 @@
 import './Experience.css';
 import drdo from '../assets/drdo.jpg';
 import ericsson from '../assets/Ericsson.png';
-import coolage from '../assets/coolage.png'
-function Experience(){
+
+function Experience() {
     return (
-        <div className="experience-section" id='experience'>
-            <span className='experience-s-heading'>Work Experience</span>
-
-            <div className='vertical-timeline vertical-timeline--animate vertical-timeline--two-columns'>
-                <div className='vertical-timeline-element'>
-                <div id="" class="vertical-timeline-element">
-                    <span class="vertical-timeline-element-icon bounce-in" style={{'border':'2px solid black','borderRadius':'50%','backgroundColor':'white','color':'black','padding':'5px'}}>
-                        <svg stroke="currentColor" fill="none" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd" clip-rule="evenodd" d="M17 7C17 5.34315 15.6569 4 14 4H10C8.34315 4 7 5.34315 7 7H6C4.34315 7 3 8.34315 3 10V18C3 19.6569 4.34315 21 6 21H18C19.6569 21 21 19.6569 21 18V10C21 8.34315 19.6569 7 18 7H17ZM14 6H10C9.44772 6 9 6.44772 9 7H15C15 6.44772 14.5523 6 14 6ZM6 9H18C18.5523 9 19 9.44772 19 10V18C19 18.5523 18.5523 19 18 19H6C5.44772 19 5 18.5523 5 18V10C5 9.44772 5.44772 9 6 9Z" fill="currentColor">
-                                </path>
-                                </svg>
-                                </span>
-                                <div class="vertical-timeline-element-content bounce-in">
-                                    <div class="vertical-timeline-element-content-arrow"></div>
-                                    <div style={{'display':'flex','alignItems':'center'}}>
-                                        <img alt='' src={ericsson} style={{'height':'50px','width':'50px'}}></img>
-                                        <div style={{display:'flex',alignItems:'baseline',flexDirection:'column'}}>
-                                        <p class="experience-designation" style={{margin:'0'}}>
-                                    Ericsson</p>
-                                    <h3 class="experience-heading" style={{margin:'0px'}}>Software Developer</h3>
-                                        </div>
-                                    </div>
-                                    
-                                    <p class="e-experience-description">
-                                    Built Ericsson’s Root Cause Analyzer. It analyses the root cause of worst performing cells & nodes of the network.
-ERCA is an internal web application, to provide solutions for issues by maintaining a set of rules for each cell.
-Built frontend features, performed end-to-end testing, and deployed on internal production pipeline
-
-                                        </p>
-                                        <span class="vertical-timeline-element-date">February 2022 - August 2023</span>
-                                        </div>
-                                        </div>
-                </div>
-{
-    /*
-    experience second
-    */
-}
-<div className='vertical-timeline-element' id="experience-2">
-<span class="vertical-timeline-element-date-2" >May 2021 - August 2021</span>
-<span  class="vertical-timeline-element-icon bounce-in-2" style={{'border':'2px solid black','borderRadius':'50%','backgroundColor':'white','color':'black','padding':'5px','marginLeft':'-130px'}}>
-                        <svg stroke="currentColor" fill="none" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd" clip-rule="evenodd" d="M17 7C17 5.34315 15.6569 4 14 4H10C8.34315 4 7 5.34315 7 7H6C4.34315 7 3 8.34315 3 10V18C3 19.6569 4.34315 21 6 21H18C19.6569 21 21 19.6569 21 18V10C21 8.34315 19.6569 7 18 7H17ZM14 6H10C9.44772 6 9 6.44772 9 7H15C15 6.44772 14.5523 6 14 6ZM6 9H18C18.5523 9 19 9.44772 19 10V18C19 18.5523 18.5523 19 18 19H6C5.44772 19 5 18.5523 5 18V10C5 9.44772 5.44772 9 6 9Z" fill="currentColor">
-                                </path>
-                                </svg>
-                                </span>
-                                       
-                <div  class="vertical-timeline-element" id="experience-2">
-                  
-                                <div class="vertical-timeline-element-content bounce-in-2" id="experience-2">
-                                    <div class="vertical-timeline-element-content-arrow" id="experience-2"></div>
-                                   <div style={{'display':'flex','alignItems':'center',gap:'15px'}}>
-<img src={drdo} style={{'height':'75px','width':'75px',borderRadius:'50%'}} alt=''></img>
-<div style={{display:'flex',alignItems:'baseline',flexDirection:'column'}} >
-<p class="experience-designation" style={{margin:'0'}}>
-DRDO, Delhi</p>
-<h3 class="experience-heading" style={{margin:'0'}}>Research Intern</h3>
-                                   
-</div>
-                                   </div>
-                                    
-                                    <p class="e-experience-description">
-                                    Researched “PRESENT Block Cipher” and built a deciphering mechanism using artificial neural networks.
-Developed a Sequential model for predicting bits of plain text at specific positions from cipher text.
-
-                                        </p>
-                                        </div>
-                                        </div>
+        <div className="experience-section" id="experience">
+            <span className="experience-s-heading">Work Experience</span>
+            <div className="experience-container">
+                {/* Ericsson Experience */}
+                <div className="experience-item">
+                    <div className="vertical-timeline-element-content bounce-in">
+                        <div className="vertical-timeline-element-content-arrow"></div>
+                        <div style={{ display: 'flex', alignItems: 'center' }}>
+                            <img alt="" src={ericsson} style={{ height: '50px', width: '50px' }} />
+                            <div style={{ display: 'flex', alignItems: 'baseline', flexDirection: 'column' }}>
+                                <p className="experience-designation" style={{ margin: '0' }}>
+                                    Ericsson (February 2020 - November 2023)
+                                </p>
+                                <h3 className="experience-heading" style={{ margin: '0px' }}>Software Developer</h3>
+                            </div>
+                        </div>
+                        <p className="e-experience-description">
+                            Built Ericsson’s Root Cause Analyzer. It analyses the root cause of worst performing cells &
+                            nodes of the network. ERCA is an internal web application, to provide solutions for issues
+                            by maintaining a set of rules for each cell. Built frontend features, performed end-to-end
+                            testing, and deployed on internal production pipeline.
+                        </p>
+                    </div>
                 </div>
 
-
-{
-    /*
-    experience third
-    */
-}
-                <div className='vertical-timeline-element'>
-                <div id="" class="vertical-timeline-element">
-                    <span  class="vertical-timeline-element-icon bounce-in" style={{'border':'2px solid black','borderRadius':'50%','backgroundColor':'white','color':'black','padding':'5px'}}>
-                        <svg stroke="currentColor" fill="none" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd" clip-rule="evenodd" d="M17 7C17 5.34315 15.6569 4 14 4H10C8.34315 4 7 5.34315 7 7H6C4.34315 7 3 8.34315 3 10V18C3 19.6569 4.34315 21 6 21H18C19.6569 21 21 19.6569 21 18V10C21 8.34315 19.6569 7 18 7H17ZM14 6H10C9.44772 6 9 6.44772 9 7H15C15 6.44772 14.5523 6 14 6ZM6 9H18C18.5523 9 19 9.44772 19 10V18C19 18.5523 18.5523 19 18 19H6C5.44772 19 5 18.5523 5 18V10C5 9.44772 5.44772 9 6 9Z" fill="currentColor">
-                                </path>
-                                </svg>
-                                </span>
-                                <div class="vertical-timeline-element-content bounce-in">
-                                    <div class="vertical-timeline-element-content-arrow"></div>
-                               <div style={{'display':'flex','alignItems':'center',gap:'15px'}}>
-                                <img alt='' src={coolage} style={{'height':'75px','width':'105px',borderRadius:'70%'}}></img>
-                                <div style={{display:'flex',alignItems:'baseline',flexDirection:'column'}}>
-                                <p class="experience-designation" style={{margin:'0'}}>
-                                    Coolage
-                                    </p>
-                                <h3 class="experience-heading" style={{margin:'0'}}>Web Developer</h3>
-                                 
-                                </div>
-                               </div>
-                                    
-                                    <p class="e-experience-description">
-Developed the main homepage of Coolage's website using HTML, CSS, and JavaScript, ensuring responsiveness across 90% of available devices.
-                                        </p>
-                                        <span class="vertical-timeline-element-date">May 2020 - June 2020</span>
-                                        </div>
-                                        </div>
+                {/* DRDO Experience */}
+                <div className="experience-item">
+                    <div className="vertical-timeline-element-content bounce-in-2">
+                        <div className="vertical-timeline-element-content-arrow"></div>
+                        <div style={{ display: 'flex', alignItems: 'center', gap: '15px' }}>
+                            <img
+                                src={drdo}
+                                style={{ height: '75px', width: '75px', borderRadius: '50%' }}
+                                alt=""
+                            />
+                            <div style={{ display: 'flex', alignItems: 'baseline', flexDirection: 'column' }}>
+                                <p className="experience-designation" style={{ margin: '0' }}>
+                                    DRDO, Delhi (May 2021 - August 2021)
+                                </p>
+                                <h3 className="experience-heading" style={{ margin: '0' }}>Research Intern</h3>
+                            </div>
+                        </div>
+                        <p className="e-experience-description">
+                            Researched “PRESENT Block Cipher” and built a deciphering mechanism using artificial neural
+                            networks. Developed a Sequential model for predicting bits of plain text at specific
+                            positions from cipher text.
+                        </p>
+                    </div>
                 </div>
             </div>
-
-            
         </div>
-    )
+    );
 }
 
 export default Experience;
